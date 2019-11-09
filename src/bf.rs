c31168a653217bccdbc00f128240a1d0d98edf37
Gordon Hart <gordon.hart2@gmail.com>
2019-11-09 01:31:12 -0800

    Beginnings of mutable program state being manipulated by commands; long way to go

--- src/bf.rs
+++ src/bf.rs
@@ -1,48 +1,76 @@
 use std::fmt;
 
 #[derive(Debug, Copy, Clone)]
-pub enum Action {
+pub enum Command {
     PtrInc,
     PtrDec,
     ValInc,
     ValDec,
-    Input,
-    Output,
-    LoopBegin,
+    PutChar,
+    GetChar,
+    LoopBeg,
     LoopEnd,
 }
 
+#[derive(Debug)]
+struct State {
+    cells: Vec<u8>,
+    ptr: u64,
+}
 
-// pub fn decode_action(action: Action) -> &'static str {
-pub fn decode_action(action: Action) -> char {
-    match action {
-        Action::PtrInc => '>',
-        Action::PtrDec => '<',
-        Action::ValInc => '+',
-        Action::ValDec => '-',
-        Action::Input => ',',
-        Action::Output => '.',
-        Action::LoopBegin => '[',
-        Action::LoopEnd => ']',
+// pub fn decode_command(command: Command) -> &'static str {
+pub fn decode_command(command: Command) -> char {
+    match command {
+        Command::PtrInc => '>',
+        Command::PtrDec => '<',
+        Command::ValInc => '+',
+        Command::ValDec => '-',
+        Command::PutChar => '.',
+        Command::GetChar => ',',
+        Command::LoopBeg => '[',
+        Command::LoopEnd => ']',
     }
 }
 
-pub fn encode_action(c: char) -> Result<Action, &'static str> {
+pub fn encode_command(c: char) -> Result<Command, String> {
     match c {
-        '>' => Ok(Action::PtrInc),
-        '<' => Ok(Action::PtrDec),
-        '+' => Ok(Action::ValInc),
-        '-' => Ok(Action::ValDec),
-        ',' => Ok(Action::Input),
-        '.' => Ok(Action::Output),
-        '[' => Ok(Action::LoopBegin),
-        ']' => Ok(Action::LoopEnd),
-        _ => Err("unrecognized"),
+        '>' => Ok(Command::PtrInc),
+        '<' => Ok(Command::PtrDec),
+        '+' => Ok(Command::ValInc),
+        '-' => Ok(Command::ValDec),
+        '.' => Ok(Command::PutChar),
+        ',' => Ok(Command::GetChar),
+        '[' => Ok(Command::LoopBeg),
+        ']' => Ok(Command::LoopEnd),
+        other => Err(format!("unsupported character: {}", other)),
     }
 }
 
-impl fmt::Display for Action {
+impl fmt::Display for Command {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result  {
-        write!(f, "{}", decode_action(*self))
+        write!(f, "{}", decode_command(*self))
+    }
+}
+
+pub fn run_program(program: &str) {
+    let mut state = State {
+        cells: Vec::new(),
+        ptr: 0,
+    };
+    for c in program.chars() {
+        match encode_command(c) {
+            Ok(command) => run_command(&mut state, command),
+            Err(message) => panic!(message),
+        };
+        println!("state: {:?}", state);
+    }
+}
+
+fn run_command(state: &mut State, cmd: Command) {
+    match cmd {
+        Command::PtrInc => state.ptr += 1,
+        Command::PtrDec => state.ptr -= 1,
+        Command::ValInc =>
+        _ => panic!("unsupported command: {}", cmd),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,12 @@ use std::env;
 mod bf;
 
 fn main() {
-    /*
-    println!("{}", bf::Action::PtrInc);
-    println!("{}", bf::decode_action(bf::Action::PtrDec));
-    println!("{}", bf::decode_action(bf::Action::ValInc));
-    */
-
+    let err_str = "usage: bf <program>";
     let args: Vec<String> = env::args().collect();
     // assert_eq!(args.len(), 2);
     match &args[..] {
-        [_, arg] => println!("{}", arg),
-        [_] => eprintln!("missing arg"),
-        _ => eprintln!("too many"),
+        [_, program] => bf::run_program(program),
+        [_] => eprintln!("missing program\n{}", err_str),
+        _ => eprintln!("too many arguments\n{}", err_str),
     }
 }
