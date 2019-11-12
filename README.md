0767423daa58970d9a41141e5e84a980eac3a9b2
Gordon Hart <gordon.hart2@gmail.com>
2019-11-11 23:10:18 -0800

    Additional commands ? (debug dump) and ! (debugging REPL) implemented, second one needs work

--- README.md
+++ README.md
@@ -14,6 +14,13 @@ $ cargo build --release
 $ ./target/release/bf <program>
 ```
 
+## Language Extensions
+This implementation follows the
+[Wikipedia standard](https://en.wikipedia.org/wiki/Brainfuck#Commands) with
+some additions:
+- `?`: Dump the internal program execution state to stderr
+- `!`: Breakpoint to enter into a Brainfuck REPL
+
 ## Goals
 - No `panic!`
 - Idiomatic language usage
@@ -21,15 +28,36 @@ $ ./target/release/bf <program>
 - Standard command line niceties:
     - Option to run program from argument
     - Option to run program from file
-    - Option to run program from stdin
+    - Read program input from stdin
     - Proper exit code setting
     - Expected options like `--help`
 - Reasonable performance and resource usage (nothing dumb)
 
 ## Example Programs
-1. `hello world`: `+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.`
-2. `Hello World!`: `++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.`
-3. `Hello World!`: `'>++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+.'`
-4. `Hello, World!`: `+[-->-[>>+>-----<<]<--<---]>-.>>>+.>>..+++[.>]<<<<.+++.------.<<-.>>>>+.`
-5. `cat` implementation: `,[.[-],]`
+1. `hello world`:
+```
++[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.
+```
+
+2. `Hello World!`:
+```
+++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+
+++.------.--------.>>+.>++.
+```
+
+3. `Hello World!`:
+```
+'>++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++
+.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+.'
+```
+
+4. `Hello, World!`:
+```
++[-->-[>>+>-----<<]<--<---]>-.>>>+.>>..+++[.>]<<<<.+++.------.<<-.>>>>+.
+```
+
+5. `cat` implementation:
+```
+,[.[-],]
+```
     - Example: `$ ./bf ',[.[-],]' < README.md`
--- src/bf.rs
+++ src/bf.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::io::Read;
+use std::io::{Write, Read};
 
 #[derive(Debug, Copy, Clone)]
 pub enum Command {
@@ -64,27 +64,21 @@ pub enum ExecutionStatus<T> {
 pub struct State {
     pub data: Vec<u8>,
     pub data_ptr: usize,
-    pub program: Vec<Command>,
     pub program_ptr: usize,
     pub loop_stack: Vec<usize>,
     pub status: ExecutionStatus<String>,
 }
 
 pub fn run_interpreter(program: &str) -> State {
-    const HEAP_SIZE: usize = 100;
     let mut state = State {
         data: vec![0], // Vec::with_capacity(HEAP_SIZE),
         data_ptr: 0,
-        program: vec![],
         program_ptr: 0,
         loop_stack: vec![],
         status: ExecutionStatus::NotStarted,
     };
     match parse_program(program) {
-        Ok(program) => {
-            state.program = program;
-            run_program(&mut state);
-        },
+        Ok(parsed_program) => run_program(&mut state, &parsed_program),
         Err(err) => state.status = ExecutionStatus::Error(err),
     };
     state
@@ -97,21 +91,16 @@ fn parse_program(program: &str) -> Result<Vec<Command>, String> {
         .collect()
 }
 
-fn run_program(state: &mut State) {
+fn run_program(state: &mut State, program: &Vec<Command>) {
     // TODO: surely there is a better way to structure this main control flow
     state.status = ExecutionStatus::InProgress;
     loop {
-        match state.program.get(state.program_ptr) {
-            Some(Command::PtrInc) => pointer_increment(state),
-            Some(Command::PtrDec) => pointer_decrement(state),
-            Some(Command::ValInc) => value_increment(state),
-            Some(Command::ValDec) => value_decrement(state),
-            Some(Command::PutChar) => put_character(state),
-            Some(Command::GetChar) => get_character(state),
-            Some(Command::LoopBeg) => loop_enter(state),
-            Some(Command::LoopEnd) => loop_exit(state),
-            Some(Command::DebugDump) => println!("{:?}", state),
-            Some(Command::DebugBreakpoint) => panic!(),
+        match state.status {
+            ExecutionStatus::Terminated | ExecutionStatus::Error(_) => break,
+            _ => {},
+        };
+        match program.get(state.program_ptr) {
+            Some(command) => run_command(state, &command, program),
             None => break,
         };
         state.program_ptr += 1;
@@ -120,6 +109,21 @@ fn run_program(state: &mut State) {
         ExecutionStatus::Error(_) => {},
         _ => state.status = ExecutionStatus::Terminated,
     }
+}
+
+fn run_command(state: &mut State, command: &Command, program: &Vec<Command>) {
+    match command {
+        Command::PtrInc => pointer_increment(state),
+        Command::PtrDec => pointer_decrement(state),
+        Command::ValInc => value_increment(state),
+        Command::ValDec => value_decrement(state),
+        Command::PutChar => put_character(state),
+        Command::GetChar => get_character(state),
+        Command::LoopBeg => loop_enter(state, program),
+        Command::LoopEnd => loop_exit(state),
+        Command::DebugDump => eprintln!("{:?}", state),
+        Command::DebugBreakpoint => debug_repl(state),
+    };
 }
 
 fn pointer_increment(state: &mut State) {
@@ -151,6 +155,7 @@ fn value_decrement(state: &mut State) {
 
 fn put_character(state: &mut State) {
     print!("{}", state.data[state.data_ptr] as char);
+    match std::io::stdout().flush() { _ => {} };
 }
 
 fn get_character(state: &mut State) {
@@ -177,9 +182,9 @@ fn find_loop_end(ptr: usize, program: &Vec<Command>) -> Result<usize, ()> {
     }
 }
 
-fn loop_enter(state: &mut State) {
+fn loop_enter(state: &mut State, program: &Vec<Command>) {
     match state.data[state.data_ptr] {
-        0 => match find_loop_end(state.program_ptr + 1, &state.program) {
+        0 => match find_loop_end(state.program_ptr + 1, program) {
             Ok(i) => state.program_ptr = i,
             Err(_) => {
                 state.status = ExecutionStatus::Error(
@@ -193,7 +198,7 @@ fn loop_enter(state: &mut State) {
 
 fn loop_exit(state: &mut State) {
     match (state.loop_stack.pop(), state.data[state.data_ptr]) {
-        (Some(ptr_loc), 0) => {},
+        (Some(_), 0) => {},
         // account for the fact that the program pointer is going to be incremented
         (Some(ptr_loc), _) => state.program_ptr = ptr_loc - 1,
         (None, _) => {
@@ -201,5 +206,46 @@ fn loop_exit(state: &mut State) {
                 "']' missing corresponding '['".to_string()
             )
         },
+    }
+}
+
+fn debug_repl(state: &mut State) {
+    // don't quite have this working yet
+    // TODO: de-uglify -- this needs to be thought through
+    let prompt: &'static str = "$ ";
+    println!(
+"You have entered an interactive session. All regular commands are available.
+Enter 'q' to exit the session and resume program execution.");
+    let mut buffer = String::new();
+    'repl: loop {
+        print!("{}", prompt);
+        std::io::stdout().flush().unwrap();
+        buffer.retain(|_| false); // empty buffer
+        match std::io::stdin().read_line(&mut buffer) {
+            Ok(_) => {
+                let mut new_program: Vec<Command> = Vec::new();
+                let prev_program_ptr = state.program_ptr;
+                state.program_ptr = 0;
+                for c in buffer.trim().chars() {
+                    if c == 'q' {
+                        for command in &new_program {
+                            run_command(state, &command, &new_program);
+                        }
+                        state.program_ptr = prev_program_ptr;
+                        break 'repl;
+                    } else {
+                        match decode_command(c) {
+                            Ok(command) => new_program.push(command),
+                            Err(err) => eprintln!("{}", err),
+                        };
+                    };
+                };
+                for command in &new_program {
+                    run_command(state, &command, &new_program);
+                }
+                state.program_ptr = prev_program_ptr;
+            },
+            Err(_) => println!("invalid input"),
+        }
     }
 }
