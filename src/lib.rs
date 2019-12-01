f4bfbb6f05f7afef8c59cc51b97a227ba4d5f9da
Gordon Hart <gordon.hart2@gmail.com>
2019-12-01 12:05:05 -0500

    Add tests for foreign function calling exposing its failure when the input has internal NUL bytes

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,14 @@ use std::ffi::{CStr, CString};
 
 use libc::c_char;
 
+use ioctx::{IoCtx, InMemoryIoCtx};
+use interpreter::{ExecutionStatus, ExecutionContext};
+
+
 pub mod ioctx;
 pub mod interpreter;
 pub mod token;
 pub mod repl;
-
-use ioctx::{IoCtx, InMemoryIoCtx};
-use interpreter::{ExecutionStatus, ExecutionContext};
 
 
 #[derive(Debug)]
@@ -68,6 +69,8 @@ pub extern "C" fn bf_exec(
     input: *const c_char,
 ) -> BfExecResult {
     let program_str: &str = unsafe { CStr::from_ptr(program).to_str().unwrap() };
+    // TODO: the input shouldn't be read as a CStr as this prevents internal null bytes, which are
+    // perfectly valid. Should pass pointer to c_uchar and length instead of c_char ptr
     let input_slice: &[u8] = unsafe { CStr::from_ptr(input).to_bytes() };
 
     let (success, output) = match execute(program_str, input_slice) {
@@ -84,13 +87,44 @@ pub extern "C" fn bf_exec(
 
 #[cfg(test)]
 mod test {
+    extern crate rand;
+
+    use rand::RngCore;
+
     use super::*;
+
+    static ADD_PROGRAM: &str = ",>,<[->+<]>.";
 
     #[test]
     fn test_addition() {
-        let program: &str = ",>,<[->+<]>.";
-        let input: Vec<u8> = vec![3, 4];
-        let output: Vec<u8> = bf_execute(program, &input[..]).unwrap();
-        assert_eq!(output, vec![7]);
+        let mut rng = rand::thread_rng();
+        let mut input: [u8; 2] = [0; 2];
+        for _ in 0..1000 {
+            rng.fill_bytes(&mut input[..]);
+            let output: Vec<u8> = execute(ADD_PROGRAM, &input[..]).unwrap();
+            let expected_output: u8 = input[0].wrapping_add(input[1]);
+            assert_eq!(output.iter().nth(0).unwrap(), &expected_output);
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn test_foreign_addition() {
+        let mut rng = rand::thread_rng();
+        let mut input: [u8; 2] = [0; 2];
+        let program: *const c_char = CString::new(ADD_PROGRAM).unwrap().into_raw();
+        for _ in 0..1000 {
+            rng.fill_bytes(&mut input[..]);
+            let input_char_p = unsafe { CString::from_vec_unchecked(input.to_vec()).into_raw() };
+            let result: BfExecResult = bf_exec(program, input_char_p);
+            assert_eq!(result.success, 1u8);
+            let expected_output: u8 = input[0].wrapping_add(input[1]);
+            let actual_output = unsafe { CStr::from_ptr(result.output).to_bytes() };
+            if actual_output.len() != 1 {
+                panic!("expected output of length 1, got {} ({} + {})",
+                    actual_output.len(), input[0], input[1]);
+            }
+            assert_eq!(actual_output[0], expected_output);
+        }
     }
 }
